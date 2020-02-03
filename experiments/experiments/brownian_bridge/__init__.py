79bf54fd7cea80d4812576f8cc525b26491910a1
VNMabus <vnmabus@gmail.com>
2020-02-03 18:35:35 +0100

    Remove telegram observer.

--- experiments/experiments/brownian_bridge/__init__.py
+++ experiments/experiments/brownian_bridge/__init__.py
@@ -6,8 +6,8 @@ from sacred.observers import TelegramObserver
 
 experiment = Experiment(name="brownian_bridge")
 
-FILE_PATH = os.path.dirname(__file__)
-
-telegram_obs = TelegramObserver.from_config(os.path.join(FILE_PATH, '..',
-                                                         'telegram.json'))
-experiment.observers.append(telegram_obs)
+# FILE_PATH = os.path.dirname(__file__)
+# 
+# telegram_obs = TelegramObserver.from_config(os.path.join(FILE_PATH, '..',
+#                                                          'telegram.json'))
+# experiment.observers.append(telegram_obs)
--- experiments/experiments/brownian_step/__init__.py
+++ experiments/experiments/brownian_step/__init__.py
@@ -7,8 +7,8 @@ from sacred.observers import TelegramObserver
 
 experiment = Experiment(name="brownian_step")
 
-FILE_PATH = os.path.dirname(__file__)
-
-telegram_obs = TelegramObserver.from_config(os.path.join(FILE_PATH, '..',
-                                                         'telegram.json'))
-experiment.observers.append(telegram_obs)
+# FILE_PATH = os.path.dirname(__file__)
+# 
+# telegram_obs = TelegramObserver.from_config(os.path.join(FILE_PATH, '..',
+#                                                          'telegram.json'))
+# experiment.observers.append(telegram_obs)
